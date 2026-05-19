d9f364adc347b763fad0a623721dc83f9c30a92d
planetryan <ryan.eus/contacte>
2026-05-18 22:38:54 -0400

    webhook proxy in worker, embed easter egg, remove easter.js

--- _worker.js
+++ _worker.js
@@ -1,10 +1,19 @@
 export default {
   async fetch(request, env) {
     const url = new URL(request.url);
+
+    // redirect pages.dev to ryan.eus
     if (url.hostname === 'ryan-eus.pages.dev') {
       url.hostname = 'ryan.eus';
       return Response.redirect(url.toString(), 301);
     }
+
+    // forward webhook
+    if (url.pathname === '/webhook/github') {
+      const target = new Request('https://baa.ryan.eus/webhook/github', request);
+      return fetch(target);
+    }
+
     return env.ASSETS.fetch(request);
   }
 }
